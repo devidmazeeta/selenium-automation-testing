3dbdec4baa487c936c7866d1ecb7e657a1f3b60a
Devid Mazeeta <devidmazeetagf@gmail.com>
2025-01-28 21:47:19 +0530

    Added multiple browser testing

--- src/com/browser/testing/EdnueTest.java
+++ src/com/browser/testing/EdnueTest.java
@@ -1,17 +1,29 @@
 package com.browser.testing;
 
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class EdnueTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		WebDriver driver = new ChromeDriver();
+//		Selenium Manager takes care of pointing to default driver
+//		Manually pointing to a selective driver
+//		System.setProperty("webdriver.chrome.driver", "C:\\BrowserDrivers\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+
+//		System.setProperty("webdriver.gecko.driver", "C:\\BrowserDrivers\\geckodriver.exe");
+//		WebDriver driver = new FirefoxDriver();
+
+//		System.setProperty("webdriver.edge.driver", "C:\\BrowserDrivers\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 
 		// Navigate to Ednue
-		driver.get("https://ednue.com/");
+		driver.get("https://google.com/");
 
 		// Validate the page title
 		String title = driver.getTitle();
@@ -20,6 +32,10 @@ public class EdnueTest {
 		// Validate the current URL
 		String currentUrl = driver.getCurrentUrl();
 		System.out.println("Current URL: " + currentUrl);
+
+		TimeUnit.SECONDS.sleep(2);
+
+		driver.close();
 
 		// Close the browser
 		if (driver != null) {
